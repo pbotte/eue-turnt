21dbf3ebc377ffdd29b1cd2ecdd38bc1e09a6d3c
pbotte <54739261+pbotte@users.noreply.github.com>
2021-03-09 11:17:05 +0100

    Für Automat V2

--- wordpress-root/schild/vorautomat.php
+++ wordpress-root/schild/vorautomat.php
@@ -9,6 +9,8 @@ $username = "U4099678";
 $password = "...";
 $dbname = "DB4099678";
 
+$AnzGesamtSlots = 1;
+
 $RueckgabeText = "Unbekannter Text";
 $debug = false;
 //$_SESSION['BelohnungErhalten'] = false;
@@ -46,22 +48,14 @@ if (isset($_SESSION['BelohnungErhalten']) and ($_SESSION['BelohnungErhalten'] ==
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 
-        $sql = "SELECT * FROM `eue_automat_status` ORDER BY `zeit` DESC LIMIT 1";
-        $AnzahlVorhandenerSlots = 0;
+        $sql = "SELECT * FROM `eue_automat2_status` ORDER BY `zeit` DESC LIMIT 1";
         $ZuvergebenderSlot = -1;
         $SlotBefuellungsstand = array(); //Zwischenspeicherung des Eintrags, um ihn später - um einen Slot modifiziert - wieder eintragen zu können
         if ($result = $conn->query($sql)) {
             while ($row = $result->fetch_assoc()) {
-                if ($debug) printf("%d,%d,%d,%d,%d\n", $row["Slot0"], $row["Slot1"], $row["Slot2"], $row["Slot3"], $row["Slot4"] );
-                for ($i=0; $i<5; $i++) {
-                    $SlotBefuellungsstand[$i] = $row["Slot".$i];
-                    if ($row["Slot".$i]>0) {
-                        $AnzahlVorhandenerSlots++;
-                        if ($debug) echo "An Slot $i haengt noch eine Tuete.\n";
-                        $ZuvergebenderSlot = $i; //Der Teilnehmer soll eine Tuete aus diesem Slot erhalten
-                    } else {
-                        if ($debug) echo "An Slot $i haengt keine Tuete mehr.\n";
-                    }
+                if ($debug) printf("%d\n", $row["slot0"]);
+                for ($i=0; $i<$AnzGesamtSlots; $i++) {
+                    $SlotBefuellungsstand[$i] = $row["slot".$i];
                 }
             }
             $result->free();
@@ -71,48 +65,43 @@ if (isset($_SESSION['BelohnungErhalten']) and ($_SESSION['BelohnungErhalten'] ==
         }
         if ($debug) echo "\n";
 
+        $ZuvergebenderSlot = 0; //Gib aus diesem slot eine Tüte
 
-        if ($AnzahlVorhandenerSlots > 0) {
-            if ($debug) echo "Es sind noch Tueten vorhanden.\n";
-            if ($debug) echo "Du erhaeltst die Tuete aus Slot Nr: $ZuvergebenderSlot\n";
-            if ($debug) var_dump($SlotBefuellungsstand);
-            $SlotBefuellungsstand[$ZuvergebenderSlot] = 0; //Dieser Slot soll ausgeloest werden
-            $sql = "INSERT INTO `eue_automat_status` (`sessionID`, `Slot0`, `Slot1`, `Slot2`, `Slot3`, `Slot4`) VALUES ('$sessionIDStr', ";
-            for ($i=0; $i<5; $i++) {
-                if ($i >0) {
-                    $sql = $sql.",";
-                }
-                $sql = $sql."'".$SlotBefuellungsstand[$i]."' ";
-            }
-            $sql = $sql.");";
-            if ($debug) echo "SQLStr: $sql\n";
+	if ($debug) echo "Du erhaeltst die Tuete aus Slot Nr: $ZuvergebenderSlot\n";
+	if ($debug) var_dump($SlotBefuellungsstand);
+	$SlotBefuellungsstand[$ZuvergebenderSlot]++; //Dieser Slot soll ausgeloest werden
+	$sql = "INSERT INTO `eue_automat2_status` (`sessionID`, `slot0`) VALUES ('$sessionIDStr', ";
+	for ($i=0; $i<$AnzGesamtSlots; $i++) {
+		if ($i >0) {
+		    $sql = $sql.",";
+		}
+		$sql = $sql."'".$SlotBefuellungsstand[$i]."' ";
+	}
+	$sql = $sql.");";
+	if ($debug) echo "SQLStr: $sql\n";
 
-            if ($conn->query($sql)) {
-                if ($debug) echo "Erfolgreich ausgefuehrt.\n";
-                $_SESSION['BelohnungErhalten'] = true;
-                $RueckgabeText = "Belohnung erhalten. Bitte entnehmen.";
+	if ($conn->query($sql)) {
+		if ($debug) echo "Erfolgreich ausgefuehrt.\n";
+		$_SESSION['BelohnungErhalten'] = true;
+		$RueckgabeText = "Belohnung erhalten. Bitte entnehmen.";
 
-                if ($debug) echo "Zurücksetzen der Session\n";
-                //Löschen der Session, damit neue Tüten erworben werden können.
-                session_unset();     // unset $_SESSION variable for the run-time 
-                session_destroy();   // destroy session data in storage
+		if ($debug) echo "Zurücksetzen der Session\n";
+		//Löschen der Session, damit neue Tüten erworben werden können.
+		session_unset();     // unset $_SESSION variable for the run-time 
+		session_destroy();   // destroy session data in storage
 
-                ini_set('session.gc_maxlifetime', 60*60*10); // expires in 10h
-                ini_set('session.cookie_lifetime', 60*60*10); // expires in 10h
+		ini_set('session.gc_maxlifetime', 60*60*10); // expires in 10h
+		ini_set('session.cookie_lifetime', 60*60*10); // expires in 10h
 
-                session_start();
-                session_regenerate_id();
-                if ($debug) var_dump($_SESSION);
-            } else {
-                if ($debug) echo "Fehler bei der Abfrage: " . $sql . "<br>" . $conn->error;
-                $RueckgabeText = "Fehler F3 aufgetreten.";
-            }
-            if ($debug) echo "\n";
+		session_start();
+		session_regenerate_id();
+		if ($debug) var_dump($_SESSION);
+	} else {
+		if ($debug) echo "Fehler bei der Abfrage: " . $sql . "<br>" . $conn->error;
+		$RueckgabeText = "Fehler F3 aufgetreten.";
+	}
+	if ($debug) echo "\n";
 
-        } else {
-            if ($debug) echo "Leider sind keie Tueten mehr vorhanden, obwohl Du sie Dir verdient hattest. Komme bitte später wieder oder melde Dich bei uns.\n";
-            $RueckgabeText = "Leider sind keie Tueten mehr vorhanden, obwohl Du sie Dir verdient hattest. Komme bitte spaeter wieder oder melde Dich bei uns.";
-        }
 
         $conn->close();
 
